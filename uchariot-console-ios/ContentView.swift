7f2da13d12842895de1ab8f572646b1f987386ca
humandavey <davey.adams.three@gmail.com>
2025-06-18 13:53:07 -0500

    Initial Commit

---
+++ uchariot-console-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uchariot-console-ios
+//
+//  Created by Davey Adams on 6/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uchariot-console-ios/uchariot_console_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  uchariot_console_iosApp.swift
+//  uchariot-console-ios
+//
+//  Created by Davey Adams on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct uchariot_console_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
