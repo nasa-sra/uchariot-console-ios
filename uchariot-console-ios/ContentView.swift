ff44a15c1c225c8a2c97dff39fe79644d4766f10
humandavey <davey.adams.three@gmail.com>
2025-06-19 16:55:57 -0500

    initial ui & project settings

--- uchariot-console-ios/ContentView.swift
+++ uchariot-console-ios/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var ipText = "192.168.5.1"
+    @FocusState private var isFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("Moonscape")
+                .ignoresSafeArea()
+                .onTapGesture {
+                    isFocused = false
+                }
+            
+            VStack {
+                HStack {
+                    TextField("IP Address", text: $ipText)
+                        .foregroundStyle(.white)
+                        .padding(0)
+                        .keyboardType(.decimalPad)
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
+                        .multilineTextAlignment(.center)
+                        .focused($isFocused)
+                }
+                
+                Button("Connect") {
+                    isFocused = false
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
-        .padding()
     }
 }
 
